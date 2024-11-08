fb026508b6296188529fdd8fab63b291d28759ed
Guga Nobre <guganobre@live.com>
2024-11-08 08:39:43 -0300

    atualização

--- Questao1/obj/Debug/net6.0/Questao1.AssemblyInfo.cs
+++ Questao1/obj/Debug/net6.0/Questao1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Questao1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e79461376c687b7027252ac9ff7761cdad8809")]
 [assembly: System.Reflection.AssemblyProductAttribute("Questao1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Questao1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
